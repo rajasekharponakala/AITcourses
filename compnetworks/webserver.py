e8beb38b3906ed37d3ae281c3fe1c905be228a16
rajasekhar <rajasekhar@swecha.net>
2018-08-01 11:45:13 +0700

    add Python webserver assignment1 file

---
+++ compnetworks/webserver.py
@@ -0,0 +1,32 @@
+#Submission : Rajasekhar Ponakala(st119220)
+from socket import *
+import sys
+
+serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket.bind(("", 6789))
+serverSocket.listen(1)
+
+while True:
+	print 'Ready to serve...'
+	connectionSocket, addr = serverSocket.accept()
+
+	try:
+		message =  connectionSocket.recv(1024)
+		print 'Message is: ', message
+		filename = message.split()[1]
+		print 'File name is: ', filename
+		f = open(filename[1:])
+		outputdata = f.read()
+		connectionSocket.send("HTTP/1.1 200 OK\r\n\r\n")
+		for i in range(0, len(outputdata)):  
+			connectionSocket.send(outputdata[i])
+		connectionSocket.send("\r\n")
+		connectionSocket.close()
+
+	except IOError:
+		connectionSocket.send("HTTP/1.1 404 Not Found\r\n\r\n")
+		connectionSocket.send("<html><head></head><body><h1>404 Not Found</h1></body></html>\r\n")
+		connectionSocket.close()
+
+serverSocket.close()  
+
